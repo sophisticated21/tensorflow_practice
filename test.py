7439a5a7ded145d8f0e9b8c8e240bd892662216a
sophisticated21 <90183964+sophisticated21@users.noreply.github.com>
2023-07-13 11:31:51 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load the Fashion MNIST dataset
+fmnist = tf.keras.datasets.fashion_mnist
+
+# Load the training and test split of the Fashion MNIST dataset
+(training_images, training_labels), (test_images, test_labels) = fmnist.load_data()
+
+
+# You can put between 0 to 59999 here
+index = 0
+
+# Set number of characters per row when printing
+np.set_printoptions(linewidth=320)
+
+# Print the label and image
+print(f'LABEL: {training_labels[index]}')
+print(f'\nIMAGE PIXEL ARRAY:\n {training_images[index]}')
+
+# Visualize the image
+plt.imshow(training_images[index])
